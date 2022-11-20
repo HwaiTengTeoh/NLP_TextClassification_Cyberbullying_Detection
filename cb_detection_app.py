e6c9c71bb2be56b73057be41b571bb0d7155231b
Hwai Teng Teoh <73737180+HwaiTengTeoh@users.noreply.github.com>
2022-11-20 20:13:21 +0800

    Update cb_detection_app.py

--- cb_detection_app.py
+++ cb_detection_app.py
@@ -48,7 +48,7 @@ if selected == "Application":
     ################################################################
    
     # Instantiate
-    @st.cache()
+    @st.cache
     tool = language_tool_python.LanguageTool('en-US')
     # cont = Contractions(api_key="glove-twitter-100")
     
