67aaeca8f2053eb8d32515efc4e08f5e7f6d712d
Hwai Teng Teoh <73737180+HwaiTengTeoh@users.noreply.github.com>
2022-11-20 20:54:29 +0800

    Update cb_detection_app.py

--- cb_detection_app.py
+++ cb_detection_app.py
@@ -49,17 +49,17 @@ if selected =="Contact":
     st.title("Contacts")
     st.markdown("""
                 #### Teoh Hwai Teng
-                ##### ***Postgraduate, Master of Data Science Student, Faculty of Computer Science and Information Technology, University of Malaya***
+                ##### ***Postgraduate, Master of Data Science, Faculty of Computer Science and Information Technology, University of Malaya***
                 
-                Email: teoh0821@gmail.com
+                > Email: teoh0821@gmail.com
                 
-                Github: https://github.com/HwaiTengTeoh/NLP_TextClassification_Cyberbullying_Detection
+                > Github: https://github.com/HwaiTengTeoh/NLP_TextClassification_Cyberbullying_Detection
                 
                 ---
                 #### Associate Prof Dr. Kasturi Dewi A/P Varathan
                 ##### ***Department of Information System, Faculty of Computer Science and Information Technology, University of Malaya***
                 
-                Email: kasturi@um.edu.my
+                > Email: kasturi@um.edu.my
          
                 """)
 
